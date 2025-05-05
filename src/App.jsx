fb43a0c2a720c33705423b02dbe218894966f643
kundetipavan <163817327+kundetipavan@users.noreply.github.com>
2025-05-05 15:05:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './style.css';
+import Home from './home.jsx';
+import Form from './form.jsx';
+
+function App() {
+   return (
+    <>
+    <Home/>
+    <Form/>
+    </>
+   );
+}
+
+export default App;
