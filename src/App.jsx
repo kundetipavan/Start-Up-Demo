099aec8843ef30a8c7792f1bcf38901b441df783
kundetipavan <163817327+kundetipavan@users.noreply.github.com>
2025-05-05 16:35:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './home.jsx';
+import Form from './form.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
